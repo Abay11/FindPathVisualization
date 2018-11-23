fc872bead9d21b3540331a1fe9089769d13c2e31
abay11 <adygha@J.A.R.V.I.S>
2018-11-23 19:32:45 +0300

    Cell: added drawing and clearing wall

--- cell.cpp
+++ cell.cpp
@@ -1,7 +1,47 @@
 #include "cell.h"
 
+bool Cell::isSpacePressed=false;
+bool Cell::isCPressed=false;
+
 Cell::Cell(qreal x, qreal y, qreal length)
  :QGraphicsRectItem (x, y, length, length)
 {
+ setAcceptHoverEvents(true);
+ setFlag(QGraphicsItem::ItemIsFocusable, true);
+ setFocus(Qt::FocusReason::MouseFocusReason);
+}
 
+void Cell::keyPressEvent(QKeyEvent *pe)
+{
+ if(pe->key()==Qt::Key_Space)
+	{
+	isSpacePressed=!isSpacePressed;
+
+	isCPressed=false;
+	}
+ else if(pe->key()==Qt::Key_C)
+	{
+	 isCPressed=!isCPressed;
+
+	 isSpacePressed=false;
+	}
+}
+
+void Cell::hoverEnterEvent(QGraphicsSceneHoverEvent *pe)
+{
+ qDebug()<<"hover event";
+
+	if(isSpacePressed)
+	 setBrush(QBrush(Qt::black));
+	else if(isCPressed)
+	 setBrush(QBrush(Qt::white));
+
+//for modifiers
+// if(pe->modifiers() & Qt::ShiftModifier)
+//	qDebug()<<"shift";
+// else if(pe->modifiers() & Qt::ControlModifier)
+//	qDebug()<<"ctrl";
+// else
+//	qDebug()<<"no state";
+// pe->modifiers();
 }
--- cell.h
+++ cell.h
@@ -2,11 +2,23 @@
 #define CELL_H
 
 #include <QGraphicsRectItem>
+#include <QGraphicsSceneHoverEvent>
+#include <QKeyEvent>
+#include <QBrush>
+#include <QApplication>
+#include <QDebug>
 
 class Cell : public QGraphicsRectItem
 {
+private:
+ static bool isSpacePressed;
+ static bool isCPressed;
+
 public:
  Cell(qreal x, qreal y, qreal cellSideLength);
+
+ void keyPressEvent(QKeyEvent *event) override;
+ void hoverEnterEvent(QGraphicsSceneHoverEvent *pe) override;
 };
 
 #endif // CELL_H
